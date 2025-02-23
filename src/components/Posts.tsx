6733845bb6c195581cdd6a21ab772f03d2f808d6
narr07 <dinarpermadi07@gmail.com>
2025-02-23 15:56:39 +0700

    Replace Link component with NavigationLink in Posts component for improved routing

--- src/components/Posts.tsx
+++ src/components/Posts.tsx
@@ -2,6 +2,7 @@
 import {Link} from '@/i18n/routing';
 import { useTranslations } from 'next-intl';
 import { POSTS_QUERYResult } from "../../sanity.types";
+import NavigationLink from './NavigationLink';
 
 export function Posts({ posts }: { posts: POSTS_QUERYResult }) {
  
@@ -10,12 +11,12 @@ export function Posts({ posts }: { posts: POSTS_QUERYResult }) {
     <div className="grid grid-cols-1 gap-4 sm:grid-cols-2">
       {posts.map((post) => (
       <div className="flex flex-col gap-2" key={post._id}>
-        <Link
-        href={`/blog/${post?.slug?.current}`}
+        <NavigationLink
+        href={{ pathname: '/blog', query: { slug: post?.slug?.current } }}
         className="cardHover px-2 py-1.5 sm:px-4 overflow-hidden bg-brand-100 dark:bg-brand-950 divide-y divide-brand-200 dark:divide-brand-800 ring-2 ring-brand-800 rounded dark:ring-brand-800 shadow"
         >
         {post?.title}
-        </Link>
+        </NavigationLink>
       </div>
       ))}
     </div>
